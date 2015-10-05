acbceb5e6b1576bc4e7f3e6c3115f6ed3f1b28c6
skiwi <frank_skiwi@hotmail.com>
2015-10-05 18:50:35 +0200

    Added static variables for the camera servo pins.

--- ROVControl/ROVControl.ino
+++ ROVControl/ROVControl.ino
@@ -1,5 +1,8 @@
 #include <Servo.h>
 
+static int SERVO_CX_PIN = 11;
+static int SERVO_CY_PIN = 10;
+
 Servo servoX;
 Servo servoY;
 
@@ -8,8 +11,8 @@ int posX = 90;
 
 void setup() {
   // put your setup code here, to run once:
-  servoX.attach(11);
-  servoY.attach(10);
+  servoX.attach(SERVO_CX_PIN);
+  servoY.attach(SERVO_CY_PIN);
   Serial.begin(9600);
   servoX.write(posX);
   servoY.write(posY);
