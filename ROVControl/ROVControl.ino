69a6dbf3deb772105d9383d4ae64037d417fd7af
skiwi <frank_skiwi@hotmail.com>
2015-10-05 20:15:58 +0200

    Added motor controls.

--- ROVControl/ROVControl.ino
+++ ROVControl/ROVControl.ino
@@ -1,7 +1,14 @@
 #include <Servo.h>
 
+//camera pins
 static int SERVO_CX_PIN = 11;
 static int SERVO_CY_PIN = 10;
+
+//motor pins
+static int MOTOR_LF_PIN = 3;
+static int MOTOR_LB_PIN = 5;
+static int MOTOR_RF_PIN = 6;
+static int MOTOR_RB_PIN = 9;
 
 Servo servoX;
 Servo servoY;
@@ -14,27 +21,60 @@ void setup() {
   servoX.attach(SERVO_CX_PIN);
   servoY.attach(SERVO_CY_PIN);
   Serial.begin(9600);
+  
+  //set camera servos to default position
   servoX.write(posX);
   servoY.write(posY);
+  
+  //set motor pins to low
+  digitalWrite(MOTOR_LF_PIN, LOW);
+  digitalWrite(MOTOR_LB_PIN, LOW);
+  digitalWrite(MOTOR_RF_PIN, LOW);
+  digitalWrite(MOTOR_RB_PIN, LOW);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if(Serial.available() > 0) {
-    String command = Serial.readStringUntil('\n');
-    int spaceIndex = command.indexOf(' ');
-    String xy = command.substring(0, spaceIndex);
-    String amount = command.substring(spaceIndex, command.length());
-    int amountInt = amount.toInt();
+    String commandText = Serial.readStringUntil('\n');
+    int spaceIndex = commandText.indexOf(' ');
+    String command = commandText.substring(0, spaceIndex);
+    String value = commandText.substring(spaceIndex, commandText.length());
     
-    if (xy == "cx") {
-      servoX.write(180 - amountInt);
+    if (command == "cx") {
+      servoX.write(180 - value.toInt());
     }
-    else if (xy == "cy") {
-      servoY.write(180 - amountInt);
+    else if (command == "cy") {
+      servoY.write(180 - value.toInt());
+    }
+    else if (command == "ml") {
+      setLeftMotor(value.toInt());
+    }
+    else if (command == "mr") {
+      setRightMotor(value.toInt());
     }
     else {
-      Serial.println("Unrecognized command: " + command);
+      Serial.println("Unrecognized command: " + commandText);
     }
+  }
+}
+
+void setLeftMotor(int value) {
+  setMotor(value, MOTOR_LF_PIN, MOTOR_LB_PIN);
+}
+
+void setRightMotor(int value) {
+  setMotor(value, MOTOR_RF_PIN, MOTOR_RB_PIN);
+}
+
+void setMotor(int value, int forwardPin, int backwardPin) {
+  value = constrain(value, -10000, 10000);
+  digitalWrite(forwardPin, LOW);
+  digitalWrite(backwardPin, LOW);
+  if (value < 0) {
+    analogWrite(backwardPin, map(-value, 0, 10000, 0, 255));
+  }
+  else if (value > 0) {
+    analogWrite(forwardPin, map(value, 0, 10000, 0, 255));
   }
 }
